8f3be629a6f80512d20198f33f6ea0983fe7e8a6
dimondudkin <dimondudkin@gmail.com>
2018-02-23 14:53:33 +0300

    Fix main container style. Added issues to block

--- imports/ui/Components/widgets/IssueTrackingBlock.js
+++ imports/ui/Components/widgets/IssueTrackingBlock.js
@@ -4,12 +4,17 @@ import { withStyles } from 'material-ui/styles';
 import classnames from 'classnames';
 
 import Chip from 'material-ui/Chip';
-import Card, { CardHeader, CardMedia, CardContent, CardActions } from 'material-ui/Card';
+import Card, { CardHeader, CardMedia, CardContent, CardActions }
+  from 'material-ui/Card';
+import List, { ListItem, ListItemSecondaryAction, ListItemText, ListItemIcon }
+  from 'material-ui/List';
 
 import IconButton from 'material-ui/IconButton';
 import Typography from 'material-ui/Typography';
 
 import Create from 'material-ui-icons/Create';
+import Radio from 'material-ui-icons/Radio';
+import BugReport from 'material-ui-icons/BugReport';
 
 const styles = theme => ({
   card: {
@@ -28,13 +33,36 @@ class IssueTrackingBlock extends React.Component {
   render() {
     const { classes } = this.props;
 
+    const issues = [
+      { name: 'Feature', icon: <Radio />, counter: '1/2/4'},
+      { name: 'Bug', icon: <BugReport />, counter: '2/2/5' },
+      { name: 'Support', icon: <BugReport />, counter: '2/4/8' },
+      { name: 'Question', icon: <BugReport />, counter: '1/3/7' },
+      { name: 'DevOps', icon: <BugReport />, counter: '0/0/0' },
+      { name: 'Design', icon: <BugReport />, counter: '0/0/0' },
+      { name: 'Epic', icon: <BugReport />, counter: '0/0/0' },
+    ]
+
     return (
       <div>
         <Card className={classes.card}>
           <CardContent>
-            <Typography component="p">
-              Issue Tracking ....
+            <Typography variant="title">
+              Issue Tracking
             </Typography>
+            <List>
+              {issues.map(issue => (
+                <ListItem key={issue.name} dense button className={classes.listItem}>
+                  <ListItemIcon>
+                    {issue.icon}
+                  </ListItemIcon>
+                  <ListItemText primary={issue.name} />
+                  <ListItemSecondaryAction>
+                    <Chip label={issue.counter}/>
+                  </ListItemSecondaryAction>
+                </ListItem>
+              ))}
+            </List>
           </CardContent>
         </Card>
       </div>
